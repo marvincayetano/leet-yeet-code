0bbc2d8324c0ff06c94b8bfd43d5a6836db616cd
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-24 16:58:26 -0500

    🔥 Chore: remove duplicates from sorted array

--- remove_duplicates_from_sorted_list.ts
+++ remove_duplicates_from_sorted_list.ts
@@ -7,8 +7,10 @@ var deleteDuplicates = function (head) {
 
   let current = head;
   while (current.next !== null) {
+    // If next value is the same then set the next to the 2nd next
     if (current.val === current.next.val) {
       current.next = current.next.next;
+      // Otherwise go to the next iteration
     } else {
       current = current.next;
     }
