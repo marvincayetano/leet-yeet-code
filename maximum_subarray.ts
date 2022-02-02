5dde9108ab2b3ffcc73d21ab90a2ee727a9a4309
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-01 21:02:26 -0500

    🔥 Chore: Added solution for maxSubArray Kadanes solution

--- maximum_subarray.ts
+++ maximum_subarray.ts
@@ -3,5 +3,22 @@
  * @return {number}
  */
 
-// THIS QUESTION is really similar to the buy sell stock question
-var maxSubArray = function (nums) {};
+// Brute force
+var maxSubArray = function (nums) {
+  // Check for constraints
+  if (nums.length === 1) return nums[0];
+
+  const n = nums.length;
+  let maxValue = 0;
+};
+
+// Kadanes solution to maxSubArray
+function maxSubArrayKadanes(arr) {
+  let a1 = 0;
+  let a2 = arr[0];
+  arr.forEach((i, a) => {
+    a1 = Math.max(i, a1 + i);
+    a2 = Math.max(a2, a1);
+  });
+  return a2;
+}
