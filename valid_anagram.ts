166f10b7fc1a2e63462b7deb1fe24a9a5c339838
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-06 22:33:47 -0500

    🔥 Chore: Short solution valid anagram

--- valid_anagram.ts
+++ valid_anagram.ts
@@ -44,7 +44,17 @@ function isAnagram(s: string, t: string): boolean {
 
 // Short solution
 function isAnagram(s: string, t: string): boolean {
+  // they are always lowercase
+  let len = s.length,
+    fMap = new Int16Array(123);
+
   // First is what are the constraints
   // s & t lengths are between 1 and 5 * 10^4
-  // they are always lowercase
+  // Well if they are not the same length, no further loops needed
+  if (t.length !== len) return false;
+
+  // charCodeAt() is a string method that is used to retrieve a Unicode value for a character at a specific position in a string.
+  for (let i = 0; i < len; i++) fMap[s.charCodeAt(i)]++;
+  for (let i = 0; i < len; i++) if (--fMap[t.charCodeAt(i)] < 0) return false;
+  return true;
 }
