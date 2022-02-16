ab8387c5e1e3b964e36356ec5e51bc2bbbacf212
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-15 19:57:04 -0500

    🔥 Chore: valid_parenthesis Long answer

--- valid_parenthesis.ts
+++ valid_parenthesis.ts
@@ -5,4 +5,44 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 
-function isValid(s: string): boolean {}
+function isValid(s: string): boolean {
+  // This is really easy
+  // Contraints first
+
+  // Can't have closing tag
+  if (s.length === 1) return false;
+
+  const STACK: string[] = [];
+
+  for (const singleChar of s) {
+    // The idea is that if the char is opening
+
+    // we should unshift (STACK) it to an array
+
+    // If the char is closing then we should shift (REMOVE THE FIRST ELEMENT)
+
+    // THIS IS BASICALLY A STACK
+
+    switch (singleChar) {
+      case "[":
+      case "{":
+      case "(":
+        STACK.unshift(singleChar);
+        break;
+
+      case "]":
+      case "}":
+      case ")":
+        const value = STACK.shift();
+        if (singleChar === "]" && value !== "[") return false;
+        else if (singleChar === "}" && value !== "{") return false;
+        else if (singleChar === ")" && value !== "(") return false;
+
+        break;
+    }
+  }
+
+  return STACK.length === 0;
+}
+
+console.log(isValid("()"));
