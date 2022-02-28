dc7b6c1a70e3cca3cc2ec58b1a19d6c1cc5a989f
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-27 20:56:44 -0500

    🔥 Chore: Working answer

---
+++ maximum_depth_binary_tree.ts
@@ -0,0 +1,22 @@
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+var maxDepth = function (root) {
+  if (root === null) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
