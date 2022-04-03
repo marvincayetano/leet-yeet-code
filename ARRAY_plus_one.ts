ec5388dbbcb0131274be9967feac324aeec4326d
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-02 21:52:46 -0400

    🔥 Chore: Init

---
+++ ARRAY_plus_one.ts
@@ -0,0 +1,9 @@
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+function plusOne(digits: number[]): number[] {
+  if (digits.length === 0) return digits;
+}
