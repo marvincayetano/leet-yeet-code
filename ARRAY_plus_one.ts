873f58fc6920fc0a72c5fec7be67901714a10b8d
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-02 22:08:38 -0400

    📄 Note: Floor

--- ARRAY_plus_one.ts
+++ ARRAY_plus_one.ts
@@ -5,5 +5,15 @@
 // Thus, the result should be [1,2,4].
 
 function plusOne(digits: number[]): number[] {
-  if (digits.length === 0) return digits;
+  let i = digits.length - 1;
+  let val = 0;
+  let carry = 1;
+  while (i >= 0 && carry) {
+    val = digits[i] + carry;
+    carry = Math.floor(val / 10);
+    digits[i] = val % 10;
+    i--;
+  }
+  if (carry) digits.unshift(carry);
+  return digits;
 }
