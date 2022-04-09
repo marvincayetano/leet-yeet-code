f9fb1b625aab295c75328a8f427b1fd48cdb801d
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-08 22:46:19 -0400

    📄 Note: using set, it doesn't say the numbers are unique

--- LINKED_LIST_cycle.ts
+++ LINKED_LIST_cycle.ts
@@ -3,3 +3,45 @@
 // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
 // Return true if there is a cycle in the linked list. Otherwise, return false.
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  // Set variable to store distinct nodes
+  let set = new Set();
+  // for traversing, initialize current with head node
+  let current = head;
+
+  //traverse the linked list
+  while (current) {
+    if (set.has(current)) {
+      // if duplication occurs, return true
+      return true;
+    } else {
+      set.add(current);
+    }
+
+    current = current.next;
+  }
+  // traverse is completed, cycle not found
+  return false;
+}
