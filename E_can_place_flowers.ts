ff2d9412dae20959e063502a909d222983485c72
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-21 20:21:01 -0400

    🔥 Chore: initial answer

--- E_can_place_flowers.ts
+++ E_can_place_flowers.ts
@@ -17,3 +17,36 @@
 // flowerbed[i] is 0 or 1.
 // There are no two adjacent flowers in flowerbed.
 // 0 <= n <= flowerbed.length
+
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  // Constraint
+  if (flowerbed.length === 1) {
+    if (flowerbed[0] === 1) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // Easiest I can think of is to check for three following 0 numbers in flowerbed
+  // and have an iterator to count how many of those that we have in an array
+  let bedAvailableCounter = 0;
+  let counter = 0;
+  for (const flower of flowerbed) {
+    if (flower === 1) {
+      counter = 0;
+    } else {
+      counter += 1;
+      if (counter === 3) {
+        bedAvailableCounter += 1;
+        counter = 1;
+      }
+    }
+  }
+
+  console.log(bedAvailableCounter);
+  console.log(bedAvailableCounter, n);
+  if (bedAvailableCounter >= n) return true;
+
+  return false;
+}
