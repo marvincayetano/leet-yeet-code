1ae53b737674e87cb646257952860118ce0d710b
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-23 13:07:35 -0400

    📄 Note: I already did this

--- ARRAY_E_move_zero.ts
+++ ARRAY_E_move_zero.ts
@@ -1,3 +1,28 @@
 // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 // Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
+    }
+  }
+
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+}
