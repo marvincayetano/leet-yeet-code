1389a305ca1e47aa22bacb8ee60b5ea293d80411
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-30 21:24:10 -0400

    📄 Note: using object as mapper

--- STRING_E_valid_anagram.ts
+++ STRING_E_valid_anagram.ts
@@ -18,17 +18,37 @@
 
 // Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 
+// function isAnagram(s: string, t: string): boolean {
+//   // they are always lowercase
+//   let len = s.length,
+//     fMap = new Int16Array(123);
+
+//   // First is what are the constraints
+//   // s & t lengths are between 1 and 5 * 10^4
+//   // Well if they are not the same length, no further loops needed
+//   if (t.length !== len) return false;
+
+//   for (let i = 0; i < len; i++) fMap[s.charCodeAt(i)]++;
+//   for (let i = 0; i < len; i++) if (--fMap[t.charCodeAt(i)] < 0) return false;
+//   return true;
+// }
+
 function isAnagram(s: string, t: string): boolean {
   // they are always lowercase
-  let len = s.length,
-    fMap = new Int16Array(123);
+  let len = s.length;
+  let fMap = {};
 
   // First is what are the constraints
   // s & t lengths are between 1 and 5 * 10^4
   // Well if they are not the same length, no further loops needed
   if (t.length !== len) return false;
 
-  for (let i = 0; i < len; i++) fMap[s.charCodeAt(i)]++;
-  for (let i = 0; i < len; i++) if (--fMap[t.charCodeAt(i)] < 0) return false;
+  for (let i = 0; i < len; i++) fMap[s.charCodeAt(i)] += 1;
+  for (let i = 0; i < len; i++) {
+    fMap[t.charCodeAt(i)] -= 1;
+    if (fMap[t.charCodeAt(i)] < 0) {
+      return false;
+    }
+  }
   return true;
 }
