dcc5da310f9e99c466e3ab039f4d329c40ab3d56
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-04-30 21:21:08 -0400

    📄 Note: I already answered this?

--- STRING_E_valid_anagram.ts
+++ STRING_E_valid_anagram.ts
@@ -17,3 +17,18 @@
 // s and t consist of lowercase English letters.
 
 // Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+function isAnagram(s: string, t: string): boolean {
+  // they are always lowercase
+  let len = s.length,
+    fMap = new Int16Array(123);
+
+  // First is what are the constraints
+  // s & t lengths are between 1 and 5 * 10^4
+  // Well if they are not the same length, no further loops needed
+  if (t.length !== len) return false;
+
+  for (let i = 0; i < len; i++) fMap[s.charCodeAt(i)]++;
+  for (let i = 0; i < len; i++) if (--fMap[t.charCodeAt(i)] < 0) return false;
+  return true;
+}
