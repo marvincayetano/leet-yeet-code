1dc4d0a4d4a6967803337ea7813bb7879bd2a180
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-05-01 21:00:54 -0400

    📄 Note: initial answer from dev

--- SORTING_E_first_bad_version.ts
+++ SORTING_E_first_bad_version.ts
@@ -21,3 +21,26 @@
 // Constraints:
 
 // 1 <= bad <= n <= 231 - 1
+
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    let left = 1,
+      right = n;
+    while (left < right) {
+      const mid = left + Math.floor((right - left) / 2);
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return left;
+  };
+};
