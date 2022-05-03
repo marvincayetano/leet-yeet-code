84cef549fdae2b69f5addee818ef048a6c7c31fe
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-05-02 21:14:45 -0400

    📄 Note: yikes I already answered this

--- ARRAY_E_plus_one.ts
+++ ARRAY_E_plus_one.ts
@@ -29,3 +29,23 @@
 // 1 <= digits.length <= 100
 // 0 <= digits[i] <= 9
 // digits does not contain any leading 0's.
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+function plusOne(digits: number[]): number[] {
+  let i = digits.length - 1;
+  let val = 0;
+  let carry = 1;
+  while (i >= 0 && carry) {
+    val = digits[i] + carry;
+    carry = Math.floor(val / 10);
+    digits[i] = val % 10;
+    i--;
+  }
+  if (carry) digits.unshift(carry);
+  return digits;
+}
