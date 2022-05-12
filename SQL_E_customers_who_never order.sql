6fa9e13cca2914743357d6f5ead6c932c4db3320
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-05-11 20:08:01 -0400

    🔥 Chore: 10% faster answer

--- SQL_E_customers_who_never order.sql
+++ SQL_E_customers_who_never order.sql
@@ -61,3 +61,10 @@
 SELECT name as Customers
 FROM Customers
 WHERE id NOT IN(SELECT customerId FROM Orders);
+
+-- 10% faster
+
+SELECT A.Name As Customers
+FROM Customers A
+WHERE NOT EXISTS
+(SELECT B.CustomerId FROM Orders B WHERE B.CustomerId = A.Id)
