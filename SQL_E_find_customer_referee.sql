4ae4050ebaf2d88adb0cf56c80c19093bfa61b2f
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-05-14 22:01:04 -0400

    🔥 Chore: sql server COALESE

--- SQL_E_find_customer_referee.sql
+++ SQL_E_find_customer_referee.sql
@@ -44,3 +44,11 @@
 -- +------+
 
 SELECT name FROM customer WHERE referee_id = NULL OR referee_id <> 2;
+
+-- MYSQL IFNULL
+SELECT name
+FROM customer
+WHERE IFNULL(referee_id, 0) <> 2
+
+-- SQL SERVER COALESCE
+SELECT COALESCE(NULL, NULL, NULL, 'W3Schools.com', NULL, 'Example.com');
