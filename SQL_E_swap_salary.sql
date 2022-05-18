301cd25c8ce5fe759c95677b4f7841717306955f
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-05-18 15:59:56 -0400

    init

--- SQL_E_swap_salary.sql
+++ SQL_E_swap_salary.sql
@@ -1,19 +1,21 @@
--- Table: Employees
+-- Table: Salary
 
--- +-------------+---------+
--- | Column Name | Type    |
--- +-------------+---------+
--- | employee_id | int     |
--- | name        | varchar |
--- | salary      | int     |
--- +-------------+---------+
--- employee_id is the primary key for this table.
--- Each row of this table indicates the employee ID, employee name, and salary.
+-- +-------------+----------+
+-- | Column Name | Type     |
+-- +-------------+----------+
+-- | id          | int      |
+-- | name        | varchar  |
+-- | sex         | ENUM     |
+-- | salary      | int      |
+-- +-------------+----------+
+-- id is the primary key for this table.
+-- The sex column is ENUM value of type ('m', 'f').
+-- The table contains information about an employee.
 
 
--- Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+-- Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
 
--- Return the result table ordered by employee_id.
+-- Note that you must write a single update statement, do not write any select statement for this problem.
 
 -- The query result format is in the following example.
 
@@ -22,29 +24,24 @@
 -- Example 1:
 
 -- Input:
--- Employees table:
--- +-------------+---------+--------+
--- | employee_id | name    | salary |
--- +-------------+---------+--------+
--- | 2           | Meir    | 3000   |
--- | 3           | Michael | 3800   |
--- | 7           | Addilyn | 7400   |
--- | 8           | Juan    | 6100   |
--- | 9           | Kannon  | 7700   |
--- +-------------+---------+--------+
+-- Salary table:
+-- +----+------+-----+--------+
+-- | id | name | sex | salary |
+-- +----+------+-----+--------+
+-- | 1  | A    | m   | 2500   |
+-- | 2  | B    | f   | 1500   |
+-- | 3  | C    | m   | 5500   |
+-- | 4  | D    | f   | 500    |
+-- +----+------+-----+--------+
 -- Output:
--- +-------------+-------+
--- | employee_id | bonus |
--- +-------------+-------+
--- | 2           | 0     |
--- | 3           | 0     |
--- | 7           | 7400  |
--- | 8           | 0     |
--- | 9           | 7700  |
--- +-------------+-------+
+-- +----+------+-----+--------+
+-- | id | name | sex | salary |
+-- +----+------+-----+--------+
+-- | 1  | A    | f   | 2500   |
+-- | 2  | B    | m   | 1500   |
+-- | 3  | C    | f   | 5500   |
+-- | 4  | D    | m   | 500    |
+-- +----+------+-----+--------+
 -- Explanation:
--- The employees with IDs 2 and 8 get 0 bonus because they have an even employee_id.
--- The employee with ID 3 gets 0 bonus because their name starts with 'M'.
--- -- The rest of the employees get a 100% bonus.
-
-SELECT employee_id, IF(employee_id % 2 != 0 and SUBSTRING(name, 1, 1) != 'M', salary, 0) AS bonus from Employees
+-- (1, A) and (3, C) were changed from 'm' to 'f'.
+-- (2, B) and (4, D) were changed from 'f' to 'm'.
