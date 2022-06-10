684e618f5a9b862527368659c3ee08f6bbed13d0
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-10 16:15:00 -0400

    🔥 Chore: 61% answer and 5% both mysql

--- SQL_M_market_analysis_I.sql
+++ SQL_M_market_analysis_I.sql
@@ -88,3 +88,17 @@
 -- | 4         | 2018-05-21 | 0              |
 -- +-----------+------------+----------------+
 
+-- 5%
+select Users.user_id as buyer_id,Users.join_date,count(Orders.buyer_id) as orders_in_2019 from Users
+left Join Orders on Users.user_id = Orders.buyer_id
+and Orders.order_date like '2019%'
+group by users.user_id;
+
+-- 61%
+select user_id as buyer_id, join_date,
+    sum(case when order_id is not null then 1 else 0 end) as orders_in_2019
+from users
+left join orders on users.user_id = orders.buyer_id
+and year(order_date) = 2019
+group by user_id, join_date
+order by buyer_id
