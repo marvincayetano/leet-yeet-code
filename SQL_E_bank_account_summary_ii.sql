861817305b9c3c5cc6f772482d3310bb10c92000
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-12 22:38:45 -0400

    🔥 Chore: question

--- SQL_E_bank_account_summary_ii.sql
+++ SQL_E_bank_account_summary_ii.sql
@@ -1,16 +1,33 @@
--- Table: ActorDirector
+-- Table: Users
 
--- +-------------+---------+
--- | Column Name | Type    |
--- +-------------+---------+
--- | actor_id    | int     |
--- | director_id | int     |
--- | timestamp   | int     |
--- +-------------+---------+
--- timestamp is the primary key column for this table.
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | account      | int     |
+-- | name         | varchar |
+-- +--------------+---------+
+-- account is the primary key for this table.
+-- Each row of this table contains the account number of each user in the bank.
+-- There will be no two users having the same name in the table.
 
 
--- Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+-- Table: Transactions
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | trans_id      | int     |
+-- | account       | int     |
+-- | amount        | int     |
+-- | transacted_on | date    |
+-- +---------------+---------+
+-- trans_id is the primary key for this table.
+-- Each row of this table contains all changes made to all accounts.
+-- amount is positive if the user received money and negative if they transferred money.
+-- All accounts start with a balance of 0.
+
+
+-- Write an SQL query to report the name and balance of users with a balance higher than 10000. The balance of an account is equal to the sum of the amounts of all transactions involving that account.
 
 -- Return the result table in any order.
 
@@ -21,28 +38,33 @@
 -- Example 1:
 
 -- Input:
--- ActorDirector table:
--- +-------------+-------------+-------------+
--- | actor_id    | director_id | timestamp   |
--- +-------------+-------------+-------------+
--- | 1           | 1           | 0           |
--- | 1           | 1           | 1           |
--- | 1           | 1           | 2           |
--- | 1           | 2           | 3           |
--- | 1           | 2           | 4           |
--- | 2           | 1           | 5           |
--- | 2           | 1           | 6           |
--- +-------------+-------------+-------------+
+-- Users table:
+-- +------------+--------------+
+-- | account    | name         |
+-- +------------+--------------+
+-- | 900001     | Alice        |
+-- | 900002     | Bob          |
+-- | 900003     | Charlie      |
+-- +------------+--------------+
+-- Transactions table:
+-- +------------+------------+------------+---------------+
+-- | trans_id   | account    | amount     | transacted_on |
+-- +------------+------------+------------+---------------+
+-- | 1          | 900001     | 7000       |  2020-08-01   |
+-- | 2          | 900001     | 7000       |  2020-09-01   |
+-- | 3          | 900001     | -3000      |  2020-09-02   |
+-- | 4          | 900002     | 1000       |  2020-09-12   |
+-- | 5          | 900003     | 6000       |  2020-08-07   |
+-- | 6          | 900003     | 6000       |  2020-09-07   |
+-- | 7          | 900003     | -4000      |  2020-09-11   |
+-- +------------+------------+------------+---------------+
 -- Output:
--- +-------------+-------------+
--- | actor_id    | director_id |
--- +-------------+-------------+
--- | 1           | 1           |
--- +-------------+-------------+
--- Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
-
--- 20%
-select actor_id, director_id
-from ActorDirector
-group by actor_id, director_id
-having count(*) >= 3
+-- +------------+------------+
+-- | name       | balance    |
+-- +------------+------------+
+-- | Alice      | 11000      |
+-- +------------+------------+
+-- Explanation:
+-- Alice's balance is (7000 + 7000 - 3000) = 11000.
+-- Bob's balance is 1000.
+-- - Charlie's balance is (6000 + 6000 - 4000) = 8000.
