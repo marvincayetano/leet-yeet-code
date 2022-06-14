04a114ffc56c186fc62950c537c79c7872ad1533
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-14 16:28:22 -0400

    🔥 Chore: solution

--- ALGO_E_binary_search.java
+++ ALGO_E_binary_search.java
@@ -23,3 +23,17 @@
 // 971,064
 // Submissions
 // 1,759,030
+
+// Only solution
+class Solution {
+  public int search(int[] nums, int target) {
+    int pivot, left = 0, right = nums.length - 1;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      if (nums[pivot] == target) return pivot;
+      if (target < nums[pivot]) right = pivot - 1;
+      else left = pivot + 1;
+    }
+    return -1;
+  }
+}
