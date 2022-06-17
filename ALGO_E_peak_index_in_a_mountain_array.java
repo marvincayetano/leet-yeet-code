acac981b1bf8422bde812af3dc6a82f746df3d9a
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-17 15:58:58 -0400

    🔥 Chore: 100% answer from leetcode

--- ALGO_E_peak_index_in_a_mountain_array.java
+++ ALGO_E_peak_index_in_a_mountain_array.java
@@ -27,3 +27,17 @@
 // 3 <= arr.length <= 104
 // 0 <= arr[i] <= 106
 // arr is guaranteed to be a mountain array.
+
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A[mi] < A[mi + 1])
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        return lo;
+    }
+}
