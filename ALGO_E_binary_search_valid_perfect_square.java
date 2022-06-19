0776c5a9e0eae8c04ddb9de20c4978f4f5b66b21
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-18 21:44:16 -0400

    🔥 Chore: question

--- ALGO_E_binary_search_valid_perfect_square.java
+++ ALGO_E_binary_search_valid_perfect_square.java
@@ -1,43 +1,14 @@
-// Let's call an array arr a mountain if the following properties hold:
+// Given a positive integer num, write a function which returns True if num is a perfect square else False.
 
-// arr.length >= 3
-// There exists some i with 0 < i < arr.length - 1 such that:
-// arr[0] < arr[1] < ... arr[i-1] < arr[i]
-// arr[i] > arr[i+1] > ... > arr[arr.length - 1]
-// Given an integer array arr that is guaranteed to be a mountain, return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+// Follow up: Do not use any built-in library function such as sqrt.
 
 
 
 // Example 1:
 
-// Input: arr = [0,1,0]
-// Output: 1
+// Input: num = 16
+// Output: true
 // Example 2:
 
-// Input: arr = [0,2,1,0]
-// Output: 1
-// Example 3:
-
-// Input: arr = [0,10,5,2]
-// Output: 1
-
-
-// Constraints:
-
-// 3 <= arr.length <= 104
-// 0 <= arr[i] <= 106
-// arr is guaranteed to be a mountain array.
-
-class Solution {
-    public int peakIndexInMountainArray(int[] A) {
-        int lo = 0, hi = A.length - 1;
-        while (lo < hi) {
-            int mi = lo + (hi - lo) / 2;
-            if (A[mi] < A[mi + 1])
-                lo = mi + 1;
-            else
-                hi = mi;
-        }
-        return lo;
-    }
-}
+// Input: num = 14
+// Output: false
