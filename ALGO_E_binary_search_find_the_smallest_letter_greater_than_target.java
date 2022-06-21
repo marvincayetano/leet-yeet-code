93988b5fed23600484ded92d714d356c0a516e41
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-21 15:50:03 -0400

    🔥 Chore: this is leet solution

--- ALGO_E_binary_search_find_the_smallest_letter_greater_than_target.java
+++ ALGO_E_binary_search_find_the_smallest_letter_greater_than_target.java
@@ -26,3 +26,15 @@
 // letters is sorted in non-decreasing order.
 // letters contains at least two different characters.
 // target is a lowercase English letter.
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int lo = 0, hi = letters.length;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (letters[mi] <= target) lo = mi + 1;
+            else hi = mi;
+        }
+        return letters[lo % letters.length];
+    }
+}
