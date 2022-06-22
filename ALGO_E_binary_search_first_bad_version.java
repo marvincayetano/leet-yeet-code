174c8aa09796647aa2e79be7a0eaf8fdd70ab507
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-22 15:31:58 -0400

    🔥 Chore: 18%jk;w

--- ALGO_E_binary_search_first_bad_version.java
+++ ALGO_E_binary_search_first_bad_version.java
@@ -24,3 +24,26 @@
 // Constraints:
 
 // 1 <= bad <= n <= 231 - 1
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+// 18%
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        if (isBadVersion(left)) {
+            return left;
+        }
+        return left + 1;
+    }
+}
