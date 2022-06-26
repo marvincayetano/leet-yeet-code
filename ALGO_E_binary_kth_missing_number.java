6141f40b7100ae30d4a85ddfbc384612652d35a8
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-06-26 17:09:59 -0400

    🔥 Chore: 45%

--- ALGO_E_binary_kth_missing_number.java
+++ ALGO_E_binary_kth_missing_number.java
@@ -31,3 +31,16 @@
 // Accepted
 // 176,104
 // Submissions
+
+// 45%
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+         for(int i=0;i<arr.length;i++)
+        {
+            int x=arr[i]-(i+1);
+            if(x>=k)
+                return i+k;
+        }
+        return arr.length+k;
+    }
+}
