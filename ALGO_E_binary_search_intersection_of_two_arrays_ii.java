7e9853a10ccdc8344dcd014201722891a3dbdb5b
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-04 18:40:30 -0400

    🔥 Chore: 85%

--- ALGO_E_binary_search_intersection_of_two_arrays_ii.java
+++ ALGO_E_binary_search_intersection_of_two_arrays_ii.java
@@ -28,3 +28,72 @@
 // 842,424
 // Submissions
 // 1,526,179
+
+// 46%
+public int[] intersect(int[] nums1, int[] nums2) {
+
+        if(nums1 == null || nums2 == null || nums1.length==0  || nums2.length==0)
+            return new int[0];
+        // step1: Put elements in nums1 into the map
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums1)
+            if(map.containsKey(num))
+                map.put(num,map.get(num)+1);
+            else
+                map.put(num,1);
+// step 2: iterate the nums2 and get the result
+        List<Integer> result = new ArrayList<>();
+
+            for (int num : nums2) {
+                if (map.containsKey(num) && map.get(num) > 0)
+                {   result.add(num);
+                    int freq = map.get(num);
+                    freq--;
+                    map.put(num, freq);
+            }
+        }
+
+        return listToArray(result);
+    }
+
+    private int[] listToArray(List<Integer> list) {
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+
+// 85%
+class Solution {
+  public int[] intersect(int[] nums1, int[] nums2) {
+
+        Arrays.sort(nums1);
+    Arrays.sort(nums2);
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    int p1=0, p2=0;
+    while(p1<nums1.length && p2<nums2.length){
+        if(nums1[p1]<nums2[p2]){
+            p1++;
+        }else if(nums1[p1]>nums2[p2]){
+            p2++;
+        }else{
+            list.add(nums1[p1]);
+            p1++;
+            p2++;
+
+        }
+    }
+
+    int[] result = new int[list.size()];
+    int i=0;
+    while(i<list.size()){
+        result[i]=list.get(i);
+        i++;
+    }
+    return result;
+    }
+
+}
