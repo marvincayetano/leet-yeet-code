2c99128fb163d84146f5c63e1d3e8bb7377b51b8
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-06 16:00:42 -0400

    🔥 Chore: 98%

--- ALGO_E_binary_search_maximum_distance_between_a_pair_of_values.java
+++ ALGO_E_binary_search_maximum_distance_between_a_pair_of_values.java
@@ -33,3 +33,20 @@
 // 1 <= nums1.length, nums2.length <= 105
 // 1 <= nums1[i], nums2[j] <= 105
 // Both nums1 and nums2 are non-increasing.
+
+// 98%
+class Solution {
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int maxDistance = 0;
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = length1 - 1, index2 = length2 - 1;
+        while (index1 >= 0 && index2 >= 0) {
+            while (index1 >= 0 && nums1[index1] <= nums2[index2]) {
+                maxDistance = Math.max(maxDistance, index2 - index1);
+                index1--;
+            }
+            index2--;
+        }
+        return maxDistance;
+    }
+}
