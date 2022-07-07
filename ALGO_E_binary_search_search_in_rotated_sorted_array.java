3718666786a9fe0d2e54c856d79ca887bdcebb9b
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-07 15:56:45 -0400

    🔥 Chore: question

--- ALGO_E_binary_search_search_in_rotated_sorted_array.java
+++ ALGO_E_binary_search_search_in_rotated_sorted_array.java
@@ -1,39 +1,31 @@
-// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
 // You must write an algorithm with O(log n) runtime complexity.
 
+
+
 // Example 1:
 
-// Input: nums = [-1,0,3,5,9,12], target = 9
+// Input: nums = [4,5,6,7,0,1,2], target = 0
 // Output: 4
-// Explanation: 9 exists in nums and its index is 4
 // Example 2:
 
-// Input: nums = [-1,0,3,5,9,12], target = 2
+// Input: nums = [4,5,6,7,0,1,2], target = 3
 // Output: -1
-// Explanation: 2 does not exist in nums so return -1
+// Example 3:
+
+// Input: nums = [1], target = 0
+// Output: -1
+
 
 // Constraints:
 
-// 1 <= nums.length <= 104
-// -104 < nums[i], target < 104
-// All the integers in nums are unique.
-// nums is sorted in ascending order.
-// Accepted
-// 971,064
-// Submissions
-// 1,759,030
-
-// Only solution
-class Solution {
-  public int search(int[] nums, int target) {
-    int pivot, left = 0, right = nums.length - 1;
-    while (left <= right) {
-      pivot = left + (right - left) / 2;
-      if (nums[pivot] == target) return pivot;
-      if (target < nums[pivot]) right = pivot - 1;
-      else left = pivot + 1;
-    }
-    return -1;
-  }
-}
+// 1 <= nums.length <= 5000
+// -104 <= nums[i] <= 104
+// All values of nums are unique.
+// nums is an ascending array that is possibly rotated.
+// -104 <= target <= 104
