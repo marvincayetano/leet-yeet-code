38e10fdc8f4424473b286c99c65b8998855c9d6c
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-07 15:59:00 -0400

    🔥 Chore: iterative 73%

--- ALGO_E_binary_search_search_in_rotated_sorted_array.java
+++ ALGO_E_binary_search_search_in_rotated_sorted_array.java
@@ -31,7 +31,7 @@
 // -104 <= target <= 104
 
 
-// 73%
+// 73% Recursive
 class Solution {
     public int search(int[] nums, int target) {
     return binarySearch(nums, 0, nums.length-1, target);
@@ -60,4 +60,32 @@ public int binarySearch(int[] nums, int left, int right, int target){
         }
     }
 }
+}
+
+// 73% Iterative
+public int search(int[] nums, int target) {
+    int left = 0;
+    int right= nums.length-1;
+
+    while(left<=right){
+        int mid = left + (right-left)/2;
+        if(target==nums[mid])
+            return mid;
+
+        if(nums[left]<=nums[mid]){
+            if(nums[left]<=target&& target<nums[mid]){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }else{
+            if(nums[mid]<target&& target<=nums[right]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+    }
+
+    return -1;
 }
