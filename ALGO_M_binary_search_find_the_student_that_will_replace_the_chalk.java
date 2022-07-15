b61a56154bcbc8ee2336b03cc47d81dcba751881
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-15 16:27:32 -0400

    🔥 Chore: 100%

--- ALGO_M_binary_search_find_the_student_that_will_replace_the_chalk.java
+++ ALGO_M_binary_search_find_the_student_that_will_replace_the_chalk.java
@@ -62,3 +62,55 @@ class Solution {
     throw new IllegalArgumentException();
   }
 }
+
+// 33%
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        int len = chalk.length;
+        long[] preSum = new long[len];
+        preSum[0] = chalk[0];
+        for (int i = 1; i < len; i++) {
+            preSum[i] = preSum[i - 1] + chalk[i];
+        }
+
+        if (preSum[len - 1] <= k) {
+            k %= preSum[len - 1];
+        }
+        int start = 0;
+        int end = len - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (k == preSum[mid]) {
+                return mid + 1;
+            } else if (k < preSum[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
+
+// 100%
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        int sum = 0;
+        for (int i = 0; i < chalk.length; i++) {
+            sum += chalk[i];
+            k -= chalk[i];
+            if (k < 0) {
+                return i;
+            }
+        }
+
+        k %= sum;
+        for (int i = 0; i < chalk.length; i++) {
+            k -= chalk[i];
+            if (k < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
