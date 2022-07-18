ceb4f616cb34a584175836d6939a14d50906450a
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-17 20:17:27 -0400

    🔥 Chore: 99% binary search solutio

--- ALGO_M_binary_search_longest_increasing_subsequence.java
+++ ALGO_M_binary_search_longest_increasing_subsequence.java
@@ -88,3 +88,30 @@ class Solution {
 }
 
 }
+
+// 99% binary search
+class Solution {
+  public int lengthOfLIS(int[] nums) {
+    int[] dp = new int[nums.length];
+    int len = 0; // len of sequence
+
+    for(int num: nums){
+        int idx = Arrays.binarySearch(dp, 0, len, num);
+
+        //if not found, return binarySearch return -insertPosition-1
+        if(idx < 0){
+            idx = - (idx + 1);
+        }
+
+        dp[idx] = num;
+
+        //update len when insert position is at the end
+        if(idx==len){
+            len++;
+        }
+    }
+
+    return len;
+}
+
+}
