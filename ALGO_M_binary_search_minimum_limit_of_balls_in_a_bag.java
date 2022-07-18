8d355a1d38b5a7e4ce5aa5c051faa70ebea44135
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-18 15:57:17 -0400

    🔥 Chore: 42%

--- ALGO_M_binary_search_minimum_limit_of_balls_in_a_bag.java
+++ ALGO_M_binary_search_minimum_limit_of_balls_in_a_bag.java
@@ -69,3 +69,32 @@ class Solution {
         return (num - 1) / target;
     }
 }
+
+// 42% solution
+class Solution {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int low = 1;
+        int high = Integer.MAX_VALUE;
+        int result = 0;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(check(mid, nums, maxOperations)){
+                result = mid;
+                high = mid - 1;
+            }else {
+                low = mid +1 ;
+            }
+        }
+        return result;
+    }
+
+    public boolean check(int mid, int nums[], int maxOperations){
+        for(int i=0; i<nums.length; i++){
+            maxOperations = maxOperations - (nums[i]/mid);
+            if(nums[i]%mid == 0){
+                maxOperations+=1;
+            }
+        }
+        return maxOperations>=0;
+    }
+}
