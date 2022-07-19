5d965b9cae03232d14f35e6b0deac80825a5755c
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-19 15:58:10 -0400

    🔥 Chore: bruteforce solution

--- ALGO_M_binary_search_koko_eating_bananas.java
+++ ALGO_M_binary_search_koko_eating_bananas.java
@@ -31,3 +31,35 @@
 // 199,975
 // Submissions
 // 372,463
+
+// Bruteforce solution
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        // Start at an eating speed of 1.
+        int speed = 1;
+
+        while (true) {
+            // hourSpent stands for the total hour Koko spends with
+            // the given eating speed.
+            int hourSpent = 0;
+
+            // Iterate over the piles and calculate hourSpent.
+            // We increase the hourSpent by ceil(pile / speed)
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / speed);
+                if (hourSpent > h) {
+                    break;
+                }
+            }
+
+            // Check if Koko can finish all the piles within h hours,
+            // If so, return speed. Otherwise, let speed increment by
+            // 1 and repeat the previous iteration.
+            if (hourSpent <= h) {
+                return speed;
+            } else {
+                speed += 1;
+            }
+        }
+    }
+}
