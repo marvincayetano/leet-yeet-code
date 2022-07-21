03737ba6e16c0c8401fbc9506063f12682cd330e
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-21 15:47:20 -0400

    🔥 Chore: array hashmap

--- ALGO_M_binary_search_find_the_duplicate_number.java
+++ ALGO_M_binary_search_find_the_duplicate_number.java
@@ -79,3 +79,18 @@ class Solution {
         return duplicate;
     }
 }
+
+// ARRAY HASHMAP 42.83%
+class Solution {
+    public int store(int[] nums, int cur) {
+        if (cur == nums[cur])
+            return cur;
+        int nxt = nums[cur];
+        nums[cur] = cur;
+        return store(nums, nxt);
+    }
+
+    public int findDuplicate(int[] nums) {
+        return store(nums, 0);
+    }
+}
