0e52bf0fc89c35a58fe022d9ffbae1b839542e12
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-21 15:49:02 -0400

    🔥 Chore: floys what now

--- ALGO_M_binary_search_find_the_duplicate_number.java
+++ ALGO_M_binary_search_find_the_duplicate_number.java
@@ -123,3 +123,28 @@ class Solution {
         return duplicate;
     }
 }
+
+// Floyd's Tortoise and Hare (Cycle Detection) 72%
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+
+        return hare;
+    }
+}
