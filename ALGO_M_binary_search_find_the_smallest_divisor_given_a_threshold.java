1cb96ff8e21ac63623463a7aa39e6e435f02c608
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-07-22 15:48:39 -0400

    🔥 Chore: 73% solution

--- ALGO_M_binary_search_find_the_smallest_divisor_given_a_threshold.java
+++ ALGO_M_binary_search_find_the_smallest_divisor_given_a_threshold.java
@@ -54,3 +54,37 @@ class Solution {
         return left;
     }
 }
+
+// 73%
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold)  {
+        int m = 0;
+        int l = 1;
+        int r = max(nums);
+
+        while (l <= r) {
+            if (sum(nums, threshold, m = (l + r) / 2))
+                l = m + 1;
+            else r = m - 1;
+        }
+        return l;
+    }
+
+    private static int max(int[] nums) {
+        int max = 0;
+
+        for (int n : nums) {
+            max = Math.max( max, n );
+        }
+        return max;
+    }
+
+    private static boolean sum(int[] nums, int t, int d) {
+        int sum = 0;
+
+        for (int n : nums) {
+            sum += (n - 1) / d + 1;
+        }
+        return sum > t;
+    }
+}
