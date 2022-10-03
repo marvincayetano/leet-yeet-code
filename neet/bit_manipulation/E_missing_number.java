e5d92e0ff369f4b123f1be70d4d7009411c7682f
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-10-03 16:01:33 -0400

    🔥 Chore: other questions

--- neet/bit_manipulation/E_missing_number.java
+++ neet/bit_manipulation/E_missing_number.java
@@ -12,6 +12,20 @@
 // Output: 2
 // Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
 
+// Example 3:
+
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Output: 8
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 104
+// 0 <= nums[i] <= n
+// All the numbers of nums are unique.
+
 class Solution {
     public int missingNumber(int[] nums) {
 
