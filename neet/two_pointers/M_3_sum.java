8867cf2d27b9bb03ca46cb42d125b4982d54faff
marvincayetano <cayetanomarvin@gmail.com>
2022-12-03 16:18:20 -0500

    3sum done

--- neet/two_pointers/M_3_sum.java
+++ neet/two_pointers/M_3_sum.java
@@ -41,7 +41,43 @@
 // Prerequisite: Two Sum, two sum II
 
 class Solution {
+
+    //2 pointers
     public List<List<Integer>> threeSum(int[] nums) {
-      // IF we sort the array first then potentially we can use two pointers
+        // IF we sort the array first then potentially we can use two pointers
+        Arrays.sort(nums);
+        LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        ArrayList<Integer> miniSol = new ArrayList<>();
+                        miniSol.add(nums[i]);
+                        miniSol.add(nums[left]);
+                        miniSol.add(nums[right]);
+                        sol.add(miniSol);
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return sol;
     }
 }
