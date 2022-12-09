7d2dbd1b2879a2aa5251872b4eca37917332827f
marvincayetano <cayetanomarvin@gmail.com>
2022-12-09 09:12:35 -0500

    🔥 Chore: comment

--- neet/sliding_window/E_best_time_to_buy_and_sell_stock.java
+++ neet/sliding_window/E_best_time_to_buy_and_sell_stock.java
@@ -36,11 +36,13 @@ class Solution {
         int left = 0;
         int right = 1;
         int maxProfit = 0;
+
         while (right < prices.length) {
             if (prices[left] < prices[right]) {
                 maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
                 right++;
             } else {
+                // move left pointer to right pointer and increment right pointer by 1 so that we can find the next max profit
                 left = right;
                 right++;
             }
