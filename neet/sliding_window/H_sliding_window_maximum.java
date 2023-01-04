a1e2b6ff87c4efdccd1e96169a5123ece5d53b65
marvincayetano <cayetanomarvin@gmail.com>
2023-01-04 16:02:09 -0500

    🔥 Chore: init

--- neet/sliding_window/H_sliding_window_maximum.java
+++ neet/sliding_window/H_sliding_window_maximum.java
@@ -32,6 +32,6 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
-
+      // Explanation: We use a deque to keep track of the index of the elements in the current window
     }
 }
