2af9b4be35ba9bd28dc75888e6a534dcca709a84
marvincayetano <cayetanomarvin@gmail.com>
2023-01-17 15:46:14 -0500

    🔥 Chore: permutation in string

--- neet/sliding_window/E_permutation_in_string.java
+++ neet/sliding_window/E_permutation_in_string.java
@@ -23,8 +23,41 @@
 // - a way, especially one of several possible variations, in which a set or number of things can be ordered or arranged.
 // "his thoughts raced ahead to fifty different permutations of what he must do"
 
-class Solution {
-    public boolean checkInclusion(String s1, String s2) {
 
+
+// This solution just checks for matches in the hash array.
+// Watch the video
+class Solution {
+
+    public boolean checkInclusion(String s1, String s2) {
+        if (s2.length() < s1.length()) return false;
+        int[] arr = new int[26];
+        //add the values to the hash array
+        for (int i = 0; i < s1.length(); i++) {
+            arr[s1.charAt(i) - 'a']++;
+        }
+        int i = 0;
+        int j = 0;
+        //point j to it's position
+        for (; j < s1.length(); j++) {
+            arr[s2.charAt(j) - 'a']--;
+        }
+        j--;
+        if (isEmpty(arr)) return true;
+        while (j < s2.length()) {
+            arr[s2.charAt(i) - 'a']++;
+            i++;
+            j++;
+            if (j < s2.length()) arr[s2.charAt(j) - 'a']--;
+            if (isEmpty(arr)) return true;
+        }
+        return isEmpty(arr);
+    }
+
+    public boolean isEmpty(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) return false;
+        }
+        return true;
     }
 }
