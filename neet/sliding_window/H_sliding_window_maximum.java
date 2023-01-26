4a588f6865cf165e1ac869321bbaf2f392b474da
marvincayetano <cayetanomarvin@gmail.com>
2023-01-26 15:09:16 -0500

    🔥 Chore: comments

--- neet/sliding_window/H_sliding_window_maximum.java
+++ neet/sliding_window/H_sliding_window_maximum.java
@@ -33,5 +33,6 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
       // Explanation: We use a deque to keep track of the index of the elements in the current window
+      // Monotoic queue - A monotonic Queue is a data structure the elements from the front to the end is strictly either increasing or decreasing.
     }
 }
