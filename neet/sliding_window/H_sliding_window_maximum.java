c837a9da642e54ca0190e94f43989cf1df2a37f5
marvincayetano <cayetanomarvin@gmail.com>
2023-01-27 15:57:04 -0500

    🔥 Chore: comment'

--- neet/sliding_window/H_sliding_window_maximum.java
+++ neet/sliding_window/H_sliding_window_maximum.java
@@ -34,5 +34,7 @@ class Solution {
 
       // Explanation: We use a deque to keep track of the index of the elements in the current window
       // Monotoic queue - A monotonic Queue is a data structure the elements from the front to the end is strictly either increasing or decreasing.
+
+      // We use a deque to keep track of the index of the elements in the current window
     }
 }
