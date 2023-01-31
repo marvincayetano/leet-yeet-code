dd044debd5652a13d3ac48a476202fbc7caf8447
marvincayetano <cayetanomarvin@gmail.com>
2023-01-31 15:58:20 -0500

    🔥 Chore: invert binary tree

--- neet/trees/E_invert_binary_trees.java
+++ neet/trees/E_invert_binary_trees.java
@@ -22,3 +22,24 @@
 
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+
+    }
+}
