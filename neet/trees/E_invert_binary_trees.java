5c1a597ee623393f570487af2c5809ccceb427df
marvincayetano <cayetanomarvin@gmail.com>
2023-02-01 15:56:02 -0500

    🔥 Chore: solution

--- neet/trees/E_invert_binary_trees.java
+++ neet/trees/E_invert_binary_trees.java
@@ -38,8 +38,23 @@
  *     }
  * }
  */
+
+// solution
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
+    }
+}
 
+// lol solution
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
