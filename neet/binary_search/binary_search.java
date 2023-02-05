d69c683035767f68931748814dd4d285879dcfcd
marvincayetano <cayetanomarvin@gmail.com>
2023-02-05 15:53:24 -0500

    solution a long time ago

--- neet/binary_search/binary_search.java
+++ neet/binary_search/binary_search.java
@@ -22,3 +22,17 @@
 // -104 < nums[i], target < 104
 // All the integers in nums are unique.
 // nums is sorted in ascending order.
+
+// My solution before a long time ago
+class Solution {
+  public int search(int[] nums, int target) {
+    int pivot, left = 0, right = nums.length - 1;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      if (nums[pivot] == target) return pivot;
+      if (target < nums[pivot]) right = pivot - 1;
+      else left = pivot + 1;
+    }
+    return -1;
+  }
+}
