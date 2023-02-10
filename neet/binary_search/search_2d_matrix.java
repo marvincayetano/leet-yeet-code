8de50f5d27f9019aef7fb04ab9c4d64a997b0cfd
marvincayetano <cayetanomarvin@gmail.com>
2023-02-10 18:50:47 -0500

    🔥 Chore: added old solution: gp

--- neet/binary_search/search_2d_matrix.java
+++ neet/binary_search/search_2d_matrix.java
@@ -26,3 +26,26 @@
 // n == matrix[i].length
 // 1 <= m, n <= 100
 // -104 <= matrix[i][j], target <= 104
+
+// My old solution
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+ int row = matrix.length;
+        int col = matrix[0].length;
+        int l = 0;
+        int r = row * col - 1;
+
+        while(l <=r ){
+            int mid = l + (r-l)/2;
+            int val = matrix[mid/col][mid%col];
+            if(val == target) return true;
+            if(val > target){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+
+        return false;
+    }
+}
