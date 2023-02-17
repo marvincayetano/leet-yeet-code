40b29c2f82ecc8fca80605074f6fffcb9e5799ec
marvincayetano <cayetanomarvin@gmail.com>
2023-02-17 14:00:41 -0500

    🔥 Chore: added a solution will review

--- neet/binary_search/find_minimum_in_rotated_sorted_array.java
+++ neet/binary_search/find_minimum_in_rotated_sorted_array.java
@@ -40,3 +40,19 @@
 // 2.5M
 // Acceptance Rate
 // 48.8%
+
+// public class Solution {
+//     public int findMin(int[] nums) {
+//         int left = 0;
+//         int right = nums.length - 1;
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] > nums[right]) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+//         return nums[left];
+//     }
+// }
