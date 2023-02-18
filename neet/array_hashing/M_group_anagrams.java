a5ddbd53d4e57a6e6170914afb307d1194a68a29
marvincayetano <cayetanomarvin@gmail.com>
2023-02-18 15:55:51 -0500

    init

--- neet/array_hashing/M_group_anagrams.java
+++ neet/array_hashing/M_group_anagrams.java
@@ -23,3 +23,9 @@
 // 1 <= strs.length <= 104
 // 0 <= strs[i].length <= 100
 // strs[i] consists of lowercase English letters.
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+}
