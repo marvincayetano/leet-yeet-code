66a2c3005a8db37756ba61b325a98fe5ee1956cf
marvincayetano <cayetanomarvin@gmail.com>
2023-02-20 15:56:44 -0500

    🔥 Chore: init

--- neet/array_hashing/M_group_anagrams.java
+++ neet/array_hashing/M_group_anagrams.java
@@ -27,5 +27,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
+// Map to store the sorted string as key and the list of strings as value
+        // Map<String, List<String>> map = new HashMap<>();
     }
 }
