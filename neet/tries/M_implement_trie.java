bd04bac950cf7bbcb854e2ec33217441fa033dbb
marvincayetano <cayetanomarvin@gmail.com>
2023-02-21 15:49:32 -0500

    Init

--- neet/tries/M_implement_trie.java
+++ neet/tries/M_implement_trie.java
@@ -85,9 +85,3 @@ class Trie {
         """
 
 
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
