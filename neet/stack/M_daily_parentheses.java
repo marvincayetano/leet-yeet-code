f5c12660f3370721efe88762e920d09db08f5595
marvincayetano <cayetanomarvin@gmail.com>
2023-04-02 20:51:14 -0400

    Init

--- neet/stack/M_daily_parentheses.java
+++ neet/stack/M_daily_parentheses.java
@@ -25,5 +25,15 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
 
+      // 1. Create a list to store the result
+      // 2. Create a helper function to generate the result
+      // 3. The helper function takes in the current string, the number of open parentheses, and the number of closed parentheses
+      // 4. If the number of open parentheses is greater than the number of closed parentheses, then we can add a closed parentheses
+      // 5. If the number of open parentheses is less than n, then we can add an open parentheses
+      // 6. If the number of open parentheses is equal to n, then we can add a closed parentheses
+      // 7. If the number of open parentheses is equal to n and the number of closed parentheses is equal to n, then we have a valid parentheses
+      // 8. Return the result
+
+
     }
 }
