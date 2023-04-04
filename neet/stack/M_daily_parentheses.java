efa99a026a618d7cd341c690d049a57db900bba7
marvincayetano <cayetanomarvin@gmail.com>
2023-04-03 20:49:05 -0400

    init

---
+++ neet/stack/M_daily_parentheses.java
@@ -0,0 +1,23 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+
+
+// Constraints:
+
+// 1 <= n <= 8
+// Accepted
+// 1.4M
+// Submissions
+// 1.9M
+// Acceptance Rate
+// 72.5%
