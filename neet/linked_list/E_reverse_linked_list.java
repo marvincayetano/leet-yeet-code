6d4e82b0b1febde48f918cb66dd4e3dc59cc1549
marvincayetano <cayetanomarvin@gmail.com>
2023-05-15 21:12:34 -0400

    reverse linked list

--- neet/linked_list/E_reverse_linked_list.java
+++ neet/linked_list/E_reverse_linked_list.java
@@ -1,43 +1,47 @@
-// Given the head of a singly linked list, reverse the list, and return the reversed list.
-
-
-
-// Example 1:
-
-
-// Input: head = [1,2,3,4,5]
-// Output: [5,4,3,2,1]
-// Example 2:
-
-
-// Input: head = [1,2]
-// Output: [2,1]
-// Example 3:
-
-// Input: head = []
-// Output: []
-
-
-// Constraints:
-
-// The number of nodes in the list is the range [0, 5000].
-// -5000 <= Node.val <= 5000
-
-
-// Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode reverseList(ListNode head) {
 
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  public ListNode reverseList(ListNode head) {
+    ListNode previous = null;
+    ListNode current = head;
+    ListNode next = null;
+
+    while (current != null) {
+      next = current.next;
+      // We're gonna point the current node to previous node
+      current.next = previous;
+      // Move the previous node to the current node
+      previous = current;
+      // Move the current node to the next node
+      current = next;
     }
+
+    return previous;
+  }
 }
