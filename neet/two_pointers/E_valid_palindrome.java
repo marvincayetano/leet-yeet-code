ec3d890f2e5da1a0c281f7af8cb32bbc0fd202e1
marvincayetano <cayetanomarvin@gmail.com>
2023-05-17 21:05:13 -0400

    valid palindrome

--- neet/two_pointers/E_valid_palindrome.java
+++ neet/two_pointers/E_valid_palindrome.java
@@ -1,91 +1,30 @@
-// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
-
-// Given a string s, return true if it is a palindrome, or false otherwise.
-
-
-
-// Example 1:
-
-// Input: s = "A man, a plan, a canal: Panama"
-// Output: true
-// Explanation: "amanaplanacanalpanama" is a palindrome.
-// Example 2:
-
-// Input: s = "race a car"
-// Output: false
-// Explanation: "raceacar" is not a palindrome.
-// Example 3:
-
-// Input: s = " "
-// Output: true
-// Explanation: s is an empty string "" after removing non-alphanumeric characters.
-// Since an empty string reads the same forward and backward, it is a palindrome.
-
-
-// Constraints:
-
-// 1 <= s.length <= 2 * 105
-// s consists only of printable ASCII characters.
-// Accepted
-// 1,589,617
-// Submissions
-// 3,661,957
-
-// First solution
 class Solution {
-    public boolean isPalindrome(String s) {
+  public boolean isPalindrome(String s) {
+    int i = 0;
+    int j = s.length() - 1;
 
-      if(s.length() == 0) return false;
+    while (i < j) {
+      Character start = s.charAt(i);
+      Character end = s.charAt(j);
 
-      // FIlter useless characters first and put it in this variable
-      let newStr = '';
-
-      // Filter out all the non-alphanumeric characters
-      for(let i = 0; i < s.length; i++) {
-        if(s[i].match(/[a-zA-Z0-9]/)) {
-          newStr += s[i].toLowerCase();
-        }
+      if (!Character.isLetterOrDigit(start)) {
+        i++;
+        continue;
       }
 
-      // Now we have a string with only alphanumeric characters
+      if (!Character.isLetterOrDigit(end)) {
+        j--;
+        continue;
+      }
 
-      // Check if it is a palindrome
-      if(newStr === newStr.reverse()) {
-        return true;
-      } else {
+      if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
         return false;
       }
 
-    }
-}
-
-// Using two pointers to check left and right characters
-public boolean isPalindrome(String s) {
-
-    int i = 0;
-    int j = s.length() - 1;
-    while (i < j) {
-
-        Character start = s.charAt(i);
-        Character end = s.charAt(j);
-
-        if (!Character.isLetterOrDigit(start)) {
-            i++;
-            continue;
-        }
-
-        if (!Character.isLetterOrDigit(end)) {
-            j--;
-            continue;
-        }
-
-        if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
-            return false;
-        }
-
-        i++;
-        j--;
+      i++;
+      j--;
     }
 
     return true;
+  }
 }
