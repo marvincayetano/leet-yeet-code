cbcf1f7bbf2583fd559b82297a7b7fb00cf25c6c
marvincayetano <cayetanomarvin@gmail.com>
2023-05-18 19:02:02 -0400

    this isn't pointer, this is sliding window. well

--- neet/two_pointers/E_minimum_differences_between_the_highes_and_lowest_of_k_scores.java
+++ neet/two_pointers/E_minimum_differences_between_the_highes_and_lowest_of_k_scores.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
   public int minimumDifference(int[] nums, int k) {
+    if (nums.length <= 1)
+      return 0;
 
+    int left = 0;
+    int right = k - 1;
+    int minimumDifference = Integer.MAX_VALUE;
+
+    int[] sortedNum = nums.clone();
+    Arrays.sort(sortedNum);
+
+    while (right < nums.length) {
+      minimumDifference = Math.min(minimumDifference, sortedNum[right] - sortedNum[left]);
+
+      right += 1;
+      left += 1;
+    }
+
+    return minimumDifference;
   }
 }
