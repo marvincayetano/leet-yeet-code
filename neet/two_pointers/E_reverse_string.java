a478e2671eb09e87c7a90d2f156366287b746f99
marvincayetano <cayetanomarvin@gmail.com>
2023-05-19 15:29:12 -0400

    solution using two pointers

---
+++ neet/two_pointers/E_reverse_string.java
@@ -0,0 +1,28 @@
+// Solution using two pointers
+class Solution {
+  public void reverseString(char[] s) {
+    if (s.length <= 1)
+      return;
+
+    int left = 0;
+    int right = s.length - 1;
+
+    while (left < right) {
+      // Swap left and right
+      char temp = s[left];
+      s[left] = s[right];
+      s[right] = temp;
+
+      left += 1;
+      right -= 1;
+    }
+
+  }
+}
+
+// Solution using two pointers
+class Solution {
+  public void reverseString(char[] s) {
+
+  }
+}
