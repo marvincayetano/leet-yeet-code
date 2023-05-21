600815a1a4096f6dd947eaf63cfea9062e744c73
marvincayetano <cayetanomarvin@gmail.com>
2023-05-20 20:49:50 -0400

    first try lol :

--- neet/two_pointers/E_remove_duplicates_from_sorted_array.java
+++ neet/two_pointers/E_remove_duplicates_from_sorted_array.java
@@ -1,5 +1,25 @@
 class Solution {
   public int removeDuplicates(int[] nums) {
+    if (nums.length <= 1)
+      return nums.length;
 
+    int left = 0;
+    int right = 1;
+    int counter = 1;
+
+    while (right < nums.length) {
+      if (nums[left] == nums[right]) {
+        // It's a duplicate
+        right++;
+      } else {
+        // It's not a duplicate
+        counter += 1;
+        nums[left + 1] = nums[right];
+        // We move the left pointe to the next positionk
+        left += 1;
+      }
+    }
+
+    return counter;
   }
 }
