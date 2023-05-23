6e89a12f323d21b2c3daeef32c4e4088475323f1
marvincayetano <cayetanomarvin@gmail.com>
2023-05-23 19:47:34 -0400

    init

---
+++ neet/array_hashing/E_contains_duplicate copy.java
@@ -0,0 +1,40 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// Accepted
+// 1,900,214
+// Submissions
+// 3,109,565
+
+// 97% hashmap solution
+
+import java.util.HashMap;
+
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Boolean> duplicateMap = new HashMap<Integer, Boolean>();
+    for (int i = 0; i < nums.length; i++) {
+      if (duplicateMap.containsKey(nums[i]))
+        return true;
+      duplicateMap.put(nums[i], true);
+    }
+
+    return false;
+  }
+}
