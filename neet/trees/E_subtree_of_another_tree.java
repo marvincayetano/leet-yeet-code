3e008e1fa133d7e8b0e82f135153b973e8c30385
marvincayetano <cayetanomarvin@gmail.com>
2023-05-23 16:15:43 -0400

    done but study more

--- neet/trees/E_subtree_of_another_tree.java
+++ neet/trees/E_subtree_of_another_tree.java
@@ -15,6 +15,28 @@
  */
 class Solution {
   public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    if (subRoot == null)
+      return true;
 
+    if (root == null)
+      return false;
+
+    if (isSametree(root, subRoot))
+      return true;
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+  }
+
+  public boolean isSametree(TreeNode s, TreeNode t) {
+    if (s == null && t == null)
+      return true;
+
+    if (s == null || t == null)
+      return false;
+
+    if (s.val != t.val)
+      return false;
+
+    return isSametree(s.left, t.left) && isSametree(s.right, t.right);
   }
 }
