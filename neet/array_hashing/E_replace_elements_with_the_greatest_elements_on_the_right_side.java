88d846e50263fd1b13b7a0c6e9e56632bbd37166
marvincayetano <cayetanomarvin@gmail.com>
2023-05-24 17:55:55 -0400

    confusing af

--- neet/array_hashing/E_replace_elements_with_the_greatest_elements_on_the_right_side.java
+++ neet/array_hashing/E_replace_elements_with_the_greatest_elements_on_the_right_side.java
@@ -1,4 +1,18 @@
 class Solution {
   public int[] replaceElements(int[] arr) {
+    int greatestMax = -1;
+
+    if (arr.length == 1) {
+      arr[0] = greatestMax;
+      return arr;
+    }
+
+    for (int i = arr.length - 1; i >= 0; i--) {
+      int newMax = Math.max(greatestMax, arr[i]);
+      arr[i] = greatestMax;
+      greatestMax = newMax;
+    }
+
+    return arr;
   }
 }
