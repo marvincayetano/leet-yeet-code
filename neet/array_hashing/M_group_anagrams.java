a05e200569323ce6982cca983c9ca748fb7338d7
marvincayetano <cayetanomarvin@gmail.com>
2023-05-26 19:41:55 -0400

    done done group anagram

--- neet/array_hashing/M_group_anagrams.java
+++ neet/array_hashing/M_group_anagrams.java
@@ -1,13 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 class Solution {
   public List<List<String>> groupAnagrams(String[] strs) {
     List<List<String>> groupAnagrams = new ArrayList<List<String>>();
+    if (strs.length == 0)
+      return groupAnagrams;
+
+    HashMap<String, List<String>> map = new HashMap<String, List<String>>();
 
     for (String str : strs) {
+      int alphabet[] = new int[26];
+      for (char c : str.toCharArray()) {
+        alphabet[c - 'a']++; // To get the index of the alphabet z - a = 26
+      }
+      String key = Arrays.toString(alphabet);
+      if (!map.containsKey(key))
+        map.put(key, new ArrayList<>());
+
+      map.get(key).add(str);
     }
 
+    groupAnagrams.addAll(map.values());
     return groupAnagrams;
   }
 }
