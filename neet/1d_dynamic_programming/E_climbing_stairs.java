0550b2c171ea7772e787674802e6f8729db4a6f2
marvincayetano <cayetanomarvin@gmail.com>
2023-05-31 20:05:47 -0400

    using fibonacci sequence

--- neet/1d_dynamic_programming/E_climbing_stairs.java
+++ neet/1d_dynamic_programming/E_climbing_stairs.java
@@ -1,5 +1,14 @@
 class Solution {
   public int climbStairs(int n) {
+    int left = 1;
+    int right = 1;
 
+    for (int i = 0; i < n - 1; i++) {
+      int temp = left + right;
+      left = right;
+      right = temp;
+    }
+
+    return right;
   }
 }
