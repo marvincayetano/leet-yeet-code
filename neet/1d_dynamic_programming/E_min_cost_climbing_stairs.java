a01da29f6fb228f15b8e386ceaf2faacba2d8533
marvincayetano <cayetanomarvin@gmail.com>
2023-06-05 19:38:19 -0400

    this is hard

--- neet/1d_dynamic_programming/E_min_cost_climbing_stairs.java
+++ neet/1d_dynamic_programming/E_min_cost_climbing_stairs.java
@@ -1,4 +1,5 @@
 // possible solution to this is to use a dp array to store the minimum cost to reach the ith step
+// Always do decision tree to solve dp problems
 class Solution {
   public int minCostClimbingStairs(int[] cost) {
 
