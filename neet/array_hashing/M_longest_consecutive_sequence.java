f5f3dd7ef8f379f824e3080fb12f9cffc19e64d8
marvincayetano <cayetanomarvin@gmail.com>
2023-06-19 21:56:12 -0400

    int

--- neet/array_hashing/M_longest_consecutive_sequence.java
+++ neet/array_hashing/M_longest_consecutive_sequence.java
@@ -1,5 +1,7 @@
 class Solution {
   public int longestConsecutive(int[] nums) {
     // Possible solution 1: sort the array and find the longest consecutive sequence
+    // Possible solution 2: use a hashset to store all the numbers, and find the
+    // longest consecutive sequence
   }
 }
