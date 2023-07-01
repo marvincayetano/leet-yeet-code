2a83d8dea7f2bf6756ddad6d10e328f763461d26
marvincayetano <cayetanomarvin@gmail.com>
2023-06-30 21:49:41 -0400

    init

--- neet/sliding_window/H_sliding_window_maximum.java
+++ neet/sliding_window/H_sliding_window_maximum.java
@@ -1,38 +1,12 @@
-// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
-
-// Return the max sliding window.
-
-// Example 1:
-
-// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
-// Output: [3,3,5,5,6,7]
-// Explanation:
-// Window position                Max
-// ---------------               -----
-// [1  3  -1] -3  5  3  6  7       3
-//  1 [3  -1  -3] 5  3  6  7       3
-//  1  3 [-1  -3  5] 3  6  7       5
-//  1  3  -1 [-3  5  3] 6  7       5
-//  1  3  -1  -3 [5  3  6] 7       6
-//  1  3  -1  -3  5 [3  6  7]      7
-// Example 2:
-
-// Input: nums = [1], k = 1
-// Output: [1]
-
-
-// Constraints:
-
-// 1 <= nums.length <= 105
-// -104 <= nums[i] <= 104
-// 1 <= k <= nums.length
-
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
+  public int[] maxSlidingWindow(int[] nums, int k) {
 
-      // Explanation: We use a deque to keep track of the index of the elements in the current window
-      // Monotoic queue - A monotonic Queue is a data structure the elements from the front to the end is strictly either increasing or decreasing.
+    // Explanation: We use a deque to keep track of the index of the elements in the
+    // current window
+    // Monotoic queue - A monotonic Queue is a data structure the elements from the
+    // front to the end is strictly either increasing or decreasing.
 
-      // We use a deque to keep track of the index of the elements in the current window
-    }
+    // We use a deque to keep track of the index of the elements in the current
+    // window
+  }
 }
