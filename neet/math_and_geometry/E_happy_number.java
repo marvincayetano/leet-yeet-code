1a00f4c70f777ab8cb082a4eff0fe776f5fa45cf
marvincayetano <cayetanomarvin@gmail.com>
2023-07-01 21:35:43 -0400

    init

--- neet/math_and_geometry/E_happy_number.java
+++ neet/math_and_geometry/E_happy_number.java
@@ -1,65 +1,36 @@
-// Write an algorithm to determine if a number n is happy.
-
-// A happy number is a number defined by the following process:
-
-// Starting with any positive integer, replace the number by the sum of the squares of its digits.
-// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
-// Those numbers for which this process ends in 1 are happy.
-// Return true if n is a happy number, and false if not.
-
-
-
-// Example 1:
-
-// Input: n = 19
-// Output: true
-// Explanation:
-// 12 + 92 = 82
-// 82 + 22 = 68
-// 62 + 82 = 100
-// 12 + 02 + 02 = 1
-// Example 2:
-
-// Input: n = 2
-// Output: false
-
-
-// Constraints:
-
-// 1 <= n <= 231 - 1
-
 class Solution {
-    public boolean isHappy(int n) {
-        if (n == 1 || n == -1) {
-            return true;
-        }
-
-        // We're using hashset because there's a chance that it loops infinitely
-        Set<Integer> visit = new HashSet<Integer>();
-
-        // compute square until getting duplicate value
-        while (!visit.contains(n)) {
-            visit.add(n);
-            // using helper function to compute the sum of squares
-            n = sumOfSquare(n);
-
-            // Returns true if n is a happy number, and false if not.
-            if (n == 1) return true;
-        }
-
-        return false;
+  public boolean isHappy(int n) {
+    if (n == 1 || n == -1) {
+      return true;
     }
 
-    public int sumOfSquare(int n) {
-        int output = 0;
+    // We're using hashset because there's a chance that it loops infinitely
+    Set<Integer> visit = new HashSet<Integer>();
 
-        while (n != 0) {
-            int digit = n % 10;
-            digit = digit * digit;
-            output += digit;
-            n = n / 10;
-        }
+    // compute square until getting duplicate value
+    while (!visit.contains(n)) {
+      visit.add(n);
+      // using helper function to compute the sum of squares
+      n = sumOfSquare(n);
 
-        return output;
+      // Returns true if n is a happy number, and false if not.
+      if (n == 1)
+        return true;
     }
+
+    return false;
+  }
+
+  public int sumOfSquare(int n) {
+    int output = 0;
+
+    while (n != 0) {
+      int digit = n % 10;
+      digit = digit * digit;
+      output += digit;
+      n = n / 10;
+    }
+
+    return output;
+  }
 }
