4d3300fbf803d67f29bd8927aaff170d1e945ac8
marvincayetano <cayetanomarvin@gmail.com>
2023-07-11 14:10:18 -0400

    added notes

---
+++ neet/two_pointers/M_container_with_the_most_water.java
@@ -0,0 +1,5 @@
+// This is the perfect medium for two pointers
+// Left and Right pointers
+// Whichever has the least value we get the power of that
+// We save this value to a variable and then we move the pointer
+// Which pointer? The one with the least value of the two (left and right)
