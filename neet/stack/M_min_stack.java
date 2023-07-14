480a31f725605574dbb122b11dbc70e3859746f6
marvincayetano <cayetanomarvin@gmail.com>
2023-07-14 09:59:17 -0400

    min stack done

--- neet/stack/M_min_stack.java
+++ neet/stack/M_min_stack.java
@@ -1,22 +1,37 @@
-class MinStack {
-  // TODO: implement this using a single stack
-  public MinStack() {
+import java.util.Stack;
 
+class MinStack {
+  // We can use two stacks to implement a min stack.
+  // One stack is used to store the actual stack elements,
+  // The other stack will be used to store the minimum values. This one always has
+  // the minimum element on the top at any point of time.
+  // If we pop a value from the original we should also pop from the min stack.
+
+  Stack<Integer> stack;
+  Stack<Integer> minStack;
+
+  public MinStack() {
+    stack = new Stack<Integer>();
+    minStack = new Stack<Integer>();
   }
 
   public void push(int val) {
-
+    stack.push(val);
+    // Get the minimum value from the min stack.
+    int minVal = Math.min(val, minStack.empty() ? val : minStack.peek());
+    minStack.push(minVal);
   }
 
   public void pop() {
-
+    stack.pop();
+    minStack.pop();
   }
 
   public int top() {
-
+    return stack.peek();
   }
 
   public int getMin() {
-
+    return minStack.peek();
   }
 }
