601416a849bfaec41981f7f06715e371607d2675
marvincayetano <cayetanomarvin@gmail.com>
2023-07-18 15:54:36 -0400

    init

--- stack/M_daily_temperatures.java
+++ stack/M_daily_temperatures.java
@@ -1,3 +1,6 @@
+// We need to check the next warmer day for each day.
+// We can use a stack to store the index of the days that we need to check.
+
 class Solution {
   public int[] dailyTemperatures(int[] temperatures) {
 
