afa3a9c60968c0f207694a46d6acd9ef21f25505
marvincayetano <cayetanomarvin@gmail.com>
2023-07-24 11:28:37 -0400

    comment

--- stack/M_car_fleet.java
+++ stack/M_car_fleet.java
@@ -1,3 +1,4 @@
 // Car fleet problem
 // Possible solution: sort the cars by position, then calculate the time to
 // reach the target
+// Cars travelling next to each other is called car fleet / convoy / caravan
