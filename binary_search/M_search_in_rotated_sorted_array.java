2bf6cb2a92c5bda7eff55bf384929831c282d2fb
marvincayetano <cayetanomarvin@gmail.com>
2023-08-03 16:07:17 -0400

    added answer

--- binary_search/M_search_in_rotated_sorted_array.java
+++ binary_search/M_search_in_rotated_sorted_array.java
@@ -1,5 +1,36 @@
-public class Solution {
+// The array is sorted in ascending order and rotated at some pivot unknown to you beforehand.
+// Similar to finding the minimum value in a rotated sorted array.
+
+class Solution {
   public int search(int[] nums, int target) {
-    // Find the index of the smallest value using binary search.
+    int l = 0;
+    int r = nums.length - 1;
+
+    while (l <= r) {
+
+      int mid = (l + r) / 2;
+
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      // left sorted
+      if (nums[l] <= nums[mid]) {
+        if (target > nums[mid] || target < nums[l]) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      } else {// right sorted
+        if (target < nums[mid] || target > nums[r]) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+      }
+
+    }
+
+    return -1;
   }
 }
