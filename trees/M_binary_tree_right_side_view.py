87754b3456e5cd1b7320f36fbba470858a867007
marvincayetano <cayetanomarvin@gmail.com>
2023-08-23 11:29:17 -0400

    added comment

---
+++ trees/M_binary_tree_right_side_view.py
@@ -0,0 +1,6 @@
+"""
+  Get all the values when looking at the right side
+  What if you can view more nodes in the left side because they're not being blocked
+  Well we can count the maximum depth from the right side, then if we reach the max depth
+  we try to get the next one and just iterate using the maximum depth
+"""
