ad49f3df1120f64d3e8076aa17b72303cb7e8505
marvincayetano <cayetanomarvin@gmail.com>
2023-08-24 11:18:07 -0400

    added comment

--- trees/M_binary_tree_right_side_view.py
+++ trees/M_binary_tree_right_side_view.py
@@ -3,4 +3,6 @@
   What if you can view more nodes in the left side because they're not being blocked
   Well we can count the maximum depth from the right side, then if we reach the max depth
   we try to get the next one and just iterate using the maximum depth
+  This is DFS,
+  BFS level order traversal is easier it seems. we get the right most value everytime
 """
