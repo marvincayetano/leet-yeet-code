f205e895fa5a14545433b32d480a4692f5274115
marvincayetano <cayetanomarvin@gmail.com>
2023-08-31 15:41:25 -0400

    init

--- trees/M_count_good_notes_in_binary_tree.py
+++ trees/M_count_good_notes_in_binary_tree.py
@@ -1,3 +1,11 @@
+"""
+  good node is less than the parent
+  using preorder traversal? dfs
+  everytime we go to the next node we just send the maxvalue we have
+  we then check if that is still greater than or equal the one we currently have
+  recursive
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,3 +14,15 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+
+      def dfs(node, maxVal):
+        if not node:
+           return 0
+
+        res = 1 if node.val >= maxVal else 0
+        maxVal = max(maxVal, node.val)
+        res += dfs(node.left, maxVal)
+        res += dfs(node.right, maxVal)
+        return res
+
+      return dfs(root, root.val)
