6ab9fabc11d5462c740eb9140537e40a15640f8c
marvincayetano <cayetanomarvin@gmail.com>
2023-09-11 16:19:24 -0400

    init

--- trees/M_kth_smallest_element_in_bst.py
+++ trees/M_kth_smallest_element_in_bst.py
@@ -1,3 +1,7 @@
+"""
+  Using a stack
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,3 +10,16 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+      stack = []
+      curr = root
+
+      while stack or curr:
+          while curr:
+              stack.append(curr)
+              curr = curr.left
+
+          curr = stack.pop()
+          k -= 1
+          if k == 0:
+              return curr.val
+          curr = curr.right
