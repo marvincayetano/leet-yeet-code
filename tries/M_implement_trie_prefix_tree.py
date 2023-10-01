da851f8f89140674d22a4fc56c785f094896a3fd
marvincayetano <cayetanomarvin@gmail.com>
2023-10-01 16:08:55 -0400

    Your commit message

--- tries/M_implement_trie_prefix_tree.py
+++ tries/M_implement_trie_prefix_tree.py
@@ -28,11 +28,3 @@ class Trie(object):
         :type prefix: str
         :rtype: bool
         """
-
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
